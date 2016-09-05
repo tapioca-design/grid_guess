226cd907e63d92674b238a022c440f9b409eb04b
Sylvain Hugues-Laclef <info@tapiocadesign.com>
2016-09-05 15:56:58 +0200

    keyboard needs to be in main

--- js/main.js
+++ js/main.js
@@ -29,15 +29,46 @@ $( document ).ready(function() {
 		function roundEnd () {
 			$("main h2, main p").show();
 			body.addClass("round-end");
+
+			$('.round-end .pyramid').on('webkitAnimationEnd animationEnd', function (e) {
+			    // alert("animation ended")
+			    console.log("round end on pyramid reached the end");
+			});
+
+
+			//when keyboard disappeared, need to display none it to use its space
+			$('.round-end #keyboard').on('webkitTransitionEnd transitionEnd', function (e) {
+			    // alert("animation ended")
+			    console.log("round end on keyboard finished disappearing");
+			    //remove keyboard and extend main to its space
+			    // $(this).css("display","none");
+			    // var currentMainHeight = $("main").height();
+			    // console.log("currentMainHeight",currentMainHeight);
+			    // $("main").css("height",currentMainHeight+88);
+			});
 		}
-		setTimeout(roundEnd, 2000);
+		// setTimeout(roundEnd, 2000);
 		/*****************************/
 
 
+
+
+		$("#btn-menu-open").click(function() {
+		  	// alert( "Handler for .click() called." );
+		  	$("#menu").removeClass().addClass("open");
+		});
+
+		$("#btn-menu-close").click(function() {
+		  	// alert( "Handler for .click() called." );
+			$("#menu").removeClass().addClass("close");
+		});
+		
+
 	}
+
+
+
 	setGridHeight();
-
-
 	window.resize = function() {
 		setGridHeight();
 	}
